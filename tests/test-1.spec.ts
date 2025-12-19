4a8309bafa13ae392ecf140ab5f57795547beb06
Liliya Petillo <liliya.petillo@gmail.com>
2025-12-19 16:26:52 -0500

    Fix Allure import to allure-playwright for CI

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import * as allure from "allure-js-commons";
+import { allure } from 'allure-playwright';
 import { generateTimestampedEmail } from './utils/email';
 import { getLastUser, saveUser } from './utils/user';
 import { getLastContact, addContactWithUser } from './utils/contact';
